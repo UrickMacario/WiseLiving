12fa1ba9318c902674b70ac22bb3ddbcc84eec84
Urick <urick@casamacario.mx>
2018-04-16 19:40:03 -0500

    Updates Urick

--- src/components/notice.js
+++ src/components/notice.js
@@ -4,7 +4,7 @@ export default class Notice extends Component {
     render(){
         return(
             <div className="Notice">
-                <h3 className="Notice-text">- últimos departamentos -</h3>
+                <h3 className="Notice-text">- últimos departamentos entrega inmediata -</h3>
             </div>
         );
     }
