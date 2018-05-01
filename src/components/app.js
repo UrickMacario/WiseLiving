58ff49141f5f78843bbc2f2d558ef21d5a3d4996
@RogFed <rogelio@rogfed.ninja>
2018-05-01 13:43:17 -0500

    Remove Contact from main flow, add padding to Apartments component

--- src/components/app.js
+++ src/components/app.js
@@ -16,7 +16,6 @@ export default class App extends Component {
                 <Notice />
                 <Navigation />
                 <MainBanner reference="MainBanner"/>
-                <Contact />
                 <Apartments />
                 <Location />
                 <Ammenities />
