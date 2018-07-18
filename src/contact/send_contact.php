da1bdfe8256c76f96c9dce93b3b2a2df2323fc49
@RogFed <rogelio@rogfed.ninja>
2018-07-18 11:33:42 -0500

    Use mailgun as a mail system

--- src/contact/send_contact.php
+++ src/contact/send_contact.php
@@ -26,23 +26,27 @@ try {
     //Server settings
     $mail->SMTPDebug = 0;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->Host = 'smtp.mailgun.org';                     // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'rog.shadow.federer@gmail.com';                 // SMTP username
-    $mail->Password = 'RogFed@2015';                           // SMTP password
+    $mail->Username = 'postmaster@mg.wiseliving.com.mx';                 // SMTP username
+    $mail->Password = '0ac435d4134b1c6de8e31e7bb50eb440-8889127d-4ec721b0';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
     $mail->setFrom($clientMail, $name);
-    $mail->addAddress('contacto@wiseliving.com.mx', 'Wise Living');     // Add a recipient
+    $mail->addAddress('contacto@wiseliving.com.mx', 'WiseLiving');     // Add a recipient
+    // $mail->addAddress('rogelio@rogfed.ninja', 'Wise Living');
 
     //Content
+    $mail->WordWrap = 50;
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->setLanguage('es', '../vendor/phpmailer/language/phpmailer.lang-es.php');
     $mail->Subject = 'Contacto Página Web';
     $mail->Body    = $fullMessage;
-    $mail->send();
+    if(!$mail->send()){
+        echo 'Error: ', $mail->ErrorInfo;
+        die();
+    }
     echo 'Message has been sent';
 } catch (Exception $e) {
     echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
